f9b328de028d84c82fea7ab648fd3c9a3692b4d3
Abhishek Jha <j7.abhi@gmail.com>
2018-03-20 22:46:05 +0530

    css configurability introduced

--- src/app/modules/civic/civic.component.ts
+++ src/app/modules/civic/civic.component.ts
@@ -15,6 +15,7 @@ export class CivicComponent implements OnInit {
   Id:string;
   buttonText:string;//=environment.buttonName;
   error:any;
+  defaultStyle:boolean;
   /*
   flag index:
   1 auth-code-recieved
@@ -24,18 +25,30 @@ export class CivicComponent implements OnInit {
   after sending sign-up request using "sendSignUpRequest()" analyse flag
   retrieve jwtToken by using  getJwtToken() method
   get the error by using getError() method
+  Use the attribute default in your app-civic reference tag if you want to use default civic style button,Ensure that you have the css file in your main html file
+  otherwise you can style it using the id that is 'signupButton'
   
   */
-
+ currentClasses: {};
+ setCurrentClasses() {
+   // CSS classes: added/removed per current state of component properties
+   this.currentClasses =  {
+     'civic-button-a': this.defaultStyle,
+     'medium': this.defaultStyle
+    
+   };
+ }
 
   constructor(x:ElementRef) {
     this.flag=-1;
    this.Id= x.nativeElement.getAttribute('appId');
    this.buttonText=x.nativeElement.getAttribute('buttonLabel');
+   this.defaultStyle=x.nativeElement.hasAttribute('default');
    }
 
   ngOnInit() {
     this.civicSip=new civic.sip({appId:this.Id});
+    this.setCurrentClasses();
 
   }
   sendSignUpRequest():number{
